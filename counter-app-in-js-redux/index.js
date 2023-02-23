933249a58b3b2a551a746b3d84524632cede4fbf
tonmoy-dev <rxdevs100@gmail.com>
2023-02-23 13:03:57 +0600

    redux added to counter app

--- counter-app-in-js-redux/index.js
+++ counter-app-in-js-redux/index.js
@@ -1,3 +1,6 @@
+// Redux Patterns
+// initialState -> actions -> reducer() -> store -> getState() -> subscribe() => action dispatch ...
+
 // select elements
 const countEl = document.getElementById("count");
 const incrementEl = document.getElementById("increment");
@@ -6,15 +9,64 @@ const decrementEl = document.getElementById("decrement");
 // console.log(countEl, incrementEl, decrementEl);
 
 // initial state
-let count = 0;
-countEl.innerText = count;
+const initialState = {
+  count: 0,
+};
+// actions
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+// reducer function
+function countReducer(state = initialState, action) {
+  if (action.type === ACTIONS.INCREMENT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  } else if (action.type === ACTIONS.DECREMENT) {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+// console.log(Redux);
+// creating store for Redux
+const store = Redux.createStore(countReducer);
+// console.log(store);
+
+// render function to get state from store and update to UI
+const render = () => {
+  const state = store.getState();
+  countEl.innerText = state.count;
+};
+render();
+
+/*
+"subscribe" is a method that allows you to listen for changes to the state of your application.
+When you subscribe to the Redux store, you are essentially telling it to call a specified function every time the state changes.
+*/
+// subscribe to the store
+store.subscribe(render);
 
 // add event listeners to increment & decrement elements
 incrementEl.addEventListener("click", () => {
-  count++;
-  countEl.innerText = count;
+  // dispatch the action to the store
+  store.dispatch({
+    type: ACTIONS.INCREMENT,
+  });
+  // render();
 });
 decrementEl.addEventListener("click", () => {
-  count--;
-  countEl.innerText = count;
+  // dispatch the action to the store
+  store.dispatch({
+    type: ACTIONS.DECREMENT,
+  });
+  // render();
 });
+
+//  "dispatch" is a method that allows you to update the state of your application by dispatching actions to the Redux store.
